f53cd6d37a1971e4452225daa3d4794eee8f94f3
currylien <79573460+currylien@users.noreply.github.com>
2021-06-01 06:20:58 -0700

    Add files via upload

---
+++ demo/main.cpp
@@ -0,0 +1,63 @@
+#include "mbed.h"
+
+#define CENTER_BASE 1500
+
+BufferedSerial pc(USBTX, USBRX);
+DigitalIn encoder(D10);
+PwmOut servo(D11);
+
+Timer t;
+Thread t2;
+Ticker encoder_ticker;
+
+volatile int steps;
+volatile int last;
+
+void servo_control(int speed) {
+   if (speed > 200)       speed = 200;
+   else if (speed < -200) speed = -200;
+
+   servo=(CENTER_BASE + speed)/20000.0f;
+}
+
+void encoder_control() {
+   int value = encoder;
+   if(!last && value) steps++;
+   last = value;
+}
+
+int main() {
+
+   pc.set_baud(9600);
+
+   encoder_ticker.attach(&encoder_control, .01);
+
+   servo.period_ms(20);
+   while(1) {
+
+   //TODO: revise this value according to your result
+   //servo_control(145.269);
+   //servo_control(28.823);
+   //TODO: revise this value according to your result
+    servo_control(-27.589);
+
+    steps = 0;
+    t.reset();
+    t.start();
+    ThisThread::sleep_for(5s);
+
+    float time = t.read();
+    printf("%1.3f\r\n", (float)steps*6.5*3.14/32/time);
+
+    servo_control(145.269);
+
+    steps = 0;
+    t.reset();
+    t.start();
+    ThisThread::sleep_for(5s);
+
+    time = t.read();
+    printf("%1.3f\r\n", (float)steps*6.5*3.14/32/time);
+
+   }
+}
